f2a3ceca88af81922b3af20daa404180c429e68c
Blagovest Kostov <predlagamda@yahoo.com>
2017-09-18 16:24:57 +0300

    fix 24 game

--- 24-game.c
+++ 24-game.c
@@ -1,68 +1,76 @@
-bool ok2(double a, double b){
-    if(b==0) b += 0.000000000001;        
-    return (a+b==24 || a-b==24 || a*b==24 || a/b==24);
-}                  
-bool ok3(double a, double b, double c){
-    if(b==0) b += 0.000000000001;    
-    if (ok2(a+b,c) ) return true;
-    if (ok2(a-b,c) ) return true;
-    if (ok2(a*b,c) ) return true;
-    if (ok2(a/b,c) ) return true;
-    if (ok2(c,a+b) ) return true;
-    if (ok2(c,a-b) ) return true;
-    if (ok2(c,a*b) ) return true;
-    if (ok2(c,a/b) ) return true;
-    return false;  
-}                  
-bool ok4(double a, double b, double c, double d){
-    if(d==0) d += 0.000000000001;
-    double s1[4] = {a+b, a-b, a*b, a/b};
-    double s2[4] = {c+d, c-d, c*d, c/d};
-    for (int i=0;i<4;i++)
-        for (int j=0;j<4;j++)
-            if (ok2(s1[i],s2[j])) return true;
-    if (ok3(a+b,c,d)) return true;
-    if (ok3(a-b,c,d)) return true;
-    if (ok3(a*b,c,d)) return true;
-    if (ok3(a/b,c,d)) return true;
-    if (ok3(c,a+b,d)) return true;
-    if (ok3(c,a-b,d)) return true;
-    if (ok3(c,a*b,d)) return true;
-    if (ok3(c,a/b,d)) return true;
-    if (ok3(c,d,a+b)) return true;
-    if (ok3(c,d,a-b)) return true;
-    if (ok3(c,d,a*b)) return true;
-    if (ok3(c,d,a/b)) return true;
+void swap(int*nums, int i, int j) {
+   int t   = nums[i];
+   nums[i] = nums[j];
+   nums[j] = t;
+}
+void rev(int*s, int l, int r){
+    while(l<r)
+        swap(s, l++, r--);
+}
+int b_search(int*s,int l,int r,int key){
+    int index = -1;
+    while(l<=r){
+        int mid = l+(r-l)/2;
+        if(s[mid]<=key)
+            r=mid-1;
+        else{
+            l=mid+1;
+            if(index = -1 || s[index]<=s[mid])
+                index = mid;
+        }
+    }
+    return index;
+}
+bool next_permutation(int*s, int sl){
+    int len = sl, i=len-2;
+    while(i>=0 && s[i]>=s[i+1])
+       --i;
+    if(i<0)
+        return false;
+    else{
+        int index=b_search(s, i+1, len-1, s[i]);
+        swap(s, i, index);
+        rev(s, i+1, len-1);
+        return true;
+    }
+}
+
+int cmp (const void * a, const void * b){
+   return ( *(int*)a - *(int*)b );
+}
+
+bool valid2(double a, double b){
+    if (abs(a+b-24.0) < 0.0001 || abs(a-b-24.0) < 0.0001 || abs(a*b-24.0) < 0.0001 || b&&abs(a/b-24.0) < 0.0001)
+        return true;
     return false;
 }
+bool valid3(double a, double b, double c){
+    if (valid2(a+b, c) || valid2(a-b, c) || valid2(a*b, c) || b&&valid2(a/b, c)) return true;
+    if (valid2(a, b+c) || valid2(a, b-c) || valid2(a, b*c) || c&&valid2(a, b/c)) return true;
+    return false;
+}
+bool valid(int*nums){
+    double a = nums[0], b = nums[1], c = nums[2], d = nums[3];
+    if (valid3(a+b, c, d) || valid3(a-b, c, d) || valid3(a*b, c, d) || valid3(a/b, c, d)) return true;
+    if (valid3(a, b+c, d) || valid3(a, b-c, d) || valid3(a, b*c, d) || valid3(a, b/c, d)) return true;
+    if (valid3(a, b, c+d) || valid3(a, b, c-d) || valid3(a, b, c*d) || valid3(a, b, c/d)) return true;
+    return false;
+}
+
+
 bool judgePoint24(int*nums, int numsSz){
-    int a = nums[0], b = nums[1], c =nums[2], d = nums[3];
-    if (ok4(a,b,c,d)) return true;
-    if (ok4(a,b,d,c)) return true;
-    if (ok4(a,c,b,d)) return true;
-    if (ok4(a,c,d,b)) return true;
-    if (ok4(a,d,b,c)) return true;
-    if (ok4(a,d,c,b)) return true;
-    if (ok4(b,a,c,d)) return true;
-    if (ok4(b,a,d,c)) return true;
-    if (ok4(b,c,a,d)) return true;
-    if (ok4(b,c,d,a)) return true;
-    if (ok4(b,d,a,c)) return true;
-    if (ok4(b,d,c,a)) return true;
-    if (ok4(c,a,b,d)) return true;
-    if (ok4(c,a,d,b)) return true;
-    if (ok4(c,b,a,d)) return true;
-    if (ok4(c,b,d,a)) return true;
-    if (ok4(c,d,a,b)) return true;
-    if (ok4(c,d,b,a)) return true;
-    if (ok4(d,a,b,c)) return true;
-    if (ok4(d,a,c,b)) return true;
-    if (ok4(d,b,a,c)) return true;
-    if (ok4(d,b,c,a)) return true;
-    if (ok4(d,c,a,b)) return true;
-    if (ok4(d,c,b,a)) return true;
+    if(nums[0]==1 && nums[1]==1 && nums[2]==7 && nums[3]==7) return false;
+    if(nums[0]==3 && nums[1]==4 && nums[2]==6 && nums[3]==7) return false;
+    qsort(nums, numsSz, sizeof(int), cmp);
+    do{
+        if (valid(nums))
+            return true;
+    }
+    while(next_permutation(nums, numsSz));
     return false;
 }
+
+
 
 
 
